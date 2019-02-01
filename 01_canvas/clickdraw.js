b206e8d69bca2f745f4317cc4e88386e828c336e
Kenny <lli15@stuy.edu>
2019-01-31 21:44:09 -0500

    01

--- 01_canvas/clickdraw.js
+++ 01_canvas/clickdraw.js
@@ -1,7 +1,7 @@
-// Kenny Li
+// lemonwater_soda Johnson Li, Kenny Li
 // SoftDev2 pd7
-// K00 -- I See a Red Door...
-// 2019-01-30
+// K01 -- ...and I want to Paint It Better
+// 2019-01-31
 
 const clear = document.getElementById("clear");
 const toggle = document.getElementById("toggle");
@@ -10,14 +10,14 @@ const canvas = document.getElementById("slate");
 var ctx = canvas.getContext("2d")
 var state = "rectangle"
 
+
+//event listener for clear button
 clear.addEventListener("click", () => {
-  e.preventDefault();
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 });
 
-
+//event listener for toggle button
 toggle.addEventListener("click", () => {
-  e.preventDefault();
   state = state == "rectangle" ? "circle" : "rectangle";
   // if (state == "rectangle") {
   //   state = "circle"
@@ -26,17 +26,22 @@ toggle.addEventListener("click", () => {
   // }
 });
 
+
+//event listener for drawing on canvas
 canvas.addEventListener("click", (e) => {
+  //prevents the browser's default action from happening
   e.preventDefault();
   if (state == "rectangle") {
+    //defines when a new path is being drawn
     ctx.beginPath();
-    ctx.rect(e.offsetX - 5, e.offsetY - 5, 10, 10)
+    //e.offsetX and e.offsetY give the coordinates relative to the canvas
+    ctx.rect(e.offsetX, e.offsetY, 10, 10)
     ctx.stroke();
-    ctx.closePath();
   } else {
+    //defines when a new path is being drawn
     ctx.beginPath();
+    //e.offsetX and e.offsetY give the coordinates relative to the canvas
     ctx.ellipse(e.offsetX, e.offsetY, 5, 5, 0, 0, 2 * Math.PI);
     ctx.stroke();
-    ctx.closePath();
   }
 });
