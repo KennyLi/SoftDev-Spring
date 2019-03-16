6235ed4aca89e934b69d072ebd81fd0d35ff9d25
Kenny <lli15@stuy.edu>
2019-03-16 04:46:25 -0400

    11

--- 11_svg/svg01.js
+++ 11_svg/svg01.js
@@ -1,11 +1,12 @@
 // Kenny Li
 // SoftDev pd8
-// K09 -- Connect the Dots
-// 2019-03-13
+// K11 -- Ask Circles [Change || Die] ...While On The Go
+// 2019-03-18
 
 var pic = document.getElementById("vimage");
-var clear = document.getElementById("but_clear");
-var move = document.getElementById("but_move");
+var clearButton = document.getElementById("but_clear");
+var moveButton = document.getElementById("but_move");
+var starButton = document.getElementById("but_star");
 var requestID;
 
 pic.addEventListener("click", function (e) {
@@ -17,6 +18,8 @@ pic.addEventListener("click", function (e) {
     c.setAttribute("r", "10");
     c.setAttribute("fill", "blue");
     c.setAttribute("stroke", "black");
+    c.setAttribute("xVel", 1);
+    c.setAttribute("yVel", 1);
     pic.appendChild(c);
     change(c)
 });
@@ -30,37 +33,86 @@ function change(child) {
             child.setAttribute("fill", "blue");
             child.setAttribute("cx", Math.random() * 500);
             child.setAttribute("cy", Math.random() * 500);
-        };
+        }
     });
-};
+}
 
-clear.addEventListener("click", function (e) {
+var move = function () {
+    window.cancelAnimationFrame(requestID);
+    for (i = 0; i < pic.children.length; i++) {
+        child = pic.children[i];
+        var radius = 10;
+
+        var xVel = parseInt(child.getAttribute("xVel"));
+        var yVel = parseInt(child.getAttribute("yVel"));
+
+        var currX = parseInt(child.getAttribute("cx")) + xVel;
+        var currY = parseInt(child.getAttribute("cy")) + yVel;
+
+        if (currX >= (500 - radius) || currX <= 0) {
+            child.setAttribute("xVel", xVel * -1);
+        }
+        if (currY >= (500 - radius) || currY <= 0) {
+            child.setAttribute("yVel", yVel * -1);
+        }
+
+        child.setAttribute("cx", currX);
+        child.setAttribute("cy", currY);
+    }
+    requestID = window.requestAnimationFrame(move);
+}
+
+var clear = function () {
+    window.cancelAnimationFrame(requestID);
     while (pic.hasChildNodes()) {
         pic.removeChild(pic.firstChild);
     }
-});
-
-var move = function() {
-  window.cancelAnimationFrame( requestID );
-
-  var radius = 10
-  var xVel = 1;
-  var yVel = 1;
-
-  for child in pic.children {
-
-	//ctx.fillRect( rectX, rectY, rectWidth, rectHeight );
-	ctx.drawImage( logo, rectX, rectY, rectWidth, rectHeight );
-
-	if ( currX >= ( pic.offsetX - radius ) || currX <= 0 ) {
-	  xVel *= -1;
-	}
-	if ( currY >= ( pic.offsetY - radius ) || currY <= 0 ) {
-	  yVel *= -1;
-	}
-
-	rectX += xVel;
-	rectY += yVel;
 }
-	requestID = window.requestAnimationFrame( dvdLogo );
-};
+
+var star = function () {
+    clear();
+    var time = 0;
+    var points = [];
+
+    var c = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+    c.setAttribute("cx", 500);
+    c.setAttribute("cy", 250);
+    c.setAttribute("r", 10);
+    c.setAttribute("fill", "black");
+    c.setAttribute("stroke", "black");
+    c.setAttribute("xVel", 1);
+    c.setAttribute("yVel", 1);
+    pic.appendChild(c);
+
+    var line = document.createElementNS("http://www.w3.org/2000/svg", "polyline");
+    line.setAttribute("stroke", "black");
+    line.setAttribute("stroke-width", 5)
+    pic.append(line);
+
+    var animateStar = function () {
+        requestID = window.requestAnimationFrame(animateStar);
+        var currX = parametricX(time);
+        var currY = parametricY(time);
+        if (points.length == 10) {
+            points.shift();
+        }
+        points.push([currX,currY]);
+        line.setAttribute("points", points);
+        c.setAttribute("cx", currX);
+        c.setAttribute("cy", currY);
+        time += 0.1
+    }
+    animateStar();
+}
+
+var parametricX = function(time) {
+    return 100 * Math.cos(time) + 150 * Math.cos(2 * time / 3) + 250;
+}
+
+var parametricY = function(time) {
+    return 100 * Math.sin(time) - 150 * Math.sin(2 * time / 3) + 250;
+}
+
+moveButton.addEventListener("click", move);
+clearButton.addEventListener("click", clear);
+starButton.addEventListener("click", star);
